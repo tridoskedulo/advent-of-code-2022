46b2583e3d4b0e9c272d3c585f40d224020e8d4a
Tri Do <tdo@skedulo.com>
2022-12-01 22:04:28 +0700

    Day 1

---
+++ AdventOfCode2022/AdventOfCode2022/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AdventOfCode2022
+//
+//  Created by Tri Do on 01/12/2022.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ AdventOfCode2022/AdventOfCode2022/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AdventOfCode2022
+//
+//  Created by Tri Do on 01/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Day1.findTheMostCalories()
+    }
+}
+
